32f41117fcbadc8e20abfed53a4991c8f5a668b1
Anton Nazarenko <moestro_001@inbox.ru>
2019-02-26 18:39:18 +0600

    sum of int

---
+++ src/main/java/com/example/task/Second.java
@@ -0,0 +1,28 @@
+package main.java.com.example.task;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Second {
+
+    public static void main(String[] args) {
+        Second.sumSimple(new int[]{1, 3, 4, 2, 6, 5});
+    }
+
+    /**
+     * @param array of int
+     */
+    public static void sumSimple(int[] array) {
+        if (array != null) {
+
+            // https://stackoverflow.com/questions/1484347/finding-the-max-min-value-in-an-array-of-primitives-using-java
+            IntSummaryStatistics statistics = Arrays.stream(array).summaryStatistics();
+            int min = statistics.getMin();
+            int max = statistics.getMax();
+
+            int sum = IntStream.of(array).sum() - (min + max);
+
+            System.out.println(sum);
+        }
+    }
+}
